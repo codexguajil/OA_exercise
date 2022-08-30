18e96462828db610a8b5fd79b47a29cd064dee41
ed-llc <edgar.munoz@louloucup.com>
2022-08-29 17:51:40 -0700

    Completing mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oaex';
+  items: Item[] = []
+  headers = [
+    {
+      id: 1,
+      label: 'Idea'
+    },
+    {
+      id: 2,
+      label: 'Development'
+    },
+    {
+      id: 3,
+      label: 'Testing'
+    },
+    {
+      id: 4,
+      label: 'Deployment'
+    }
+  ]
+
+  ngOnChange() {
+    console.log(this.items)
+  }
+
+  addItem(e: any) {
+    this.items.push({
+      id: 1,
+      content: e.target.value,
+      forward: true
+    })
+    e.target.value = ''
+  }
+
+  moveItem(item: Item) {
+    if (item.id < 4 && item.forward === true) {
+      item.id = item.id + 1
+    } else if (item.id === 4 && item.forward === true) {
+      item.forward = false
+      item.id = item.id - 1
+    } else if (item.id > 1 && item.forward === false) {
+      item.id =  item.id - 1
+    } else {
+      item.forward = true;
+      item.id = item.id + 1
+    }
+    let index = this.items.indexOf(item)
+    this.items.splice(index, 1)
+    this.items.unshift(item)
+  }
+
 }
